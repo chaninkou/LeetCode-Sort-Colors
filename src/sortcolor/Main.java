ee880fb9141da18ffa5fd9da5d3cd3f61020a6c2
chaninkou <michaelkoulove@yahoo.com>
2020-03-02 00:23:00 -0800

    Great question on sorting a 3 numbers using counting sort and unique way

---
+++ src/sortcolor/Main.java
@@ -0,0 +1,17 @@
+package sortcolor;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] nums = {2,0,2,1,1,0,1};
+		
+		System.out.println("Input: " + Arrays.toString(nums));
+		
+		SortColorByOneTwoThreeFunction solution = new SortColorByOneTwoThreeFunction();
+		
+		solution.sortColors(nums);
+		
+		System.out.println("Solution: " + Arrays.toString(nums));
+	}
+}
---
+++ src/sortcolor/SortColorByOneTwoThreeFunction.java
@@ -0,0 +1,56 @@
+package sortcolor;
+
+public class SortColorByOneTwoThreeFunction {
+	// One pass only, perfect solution, super unique
+    public void sortColors(int[] nums) {
+        int num0 = -1;
+        int num1 = -1;
+        int num2 = -1;
+        
+        for(int i = 0; i < nums.length; i++){
+        	// Update the necessary pointers
+            if(nums[i] == 0){
+                nums[++num2] = 2;
+                nums[++num1] = 1;
+                nums[++num0] = 0;
+            } else if (nums[i] == 1){
+                nums[++num2] = 2;
+                nums[++num1] = 1;
+            } else if (nums[i] == 2){
+                nums[++num2] = 2;
+            }
+        }
+        
+    }
+    
+    // Similar to counting sort
+    public void sortColors1(int[] nums) {
+        int num0 = 0;
+        int num1 = 0;
+        int num2 = 0;
+        
+        // Count the numbers of 1,2,3 first
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                num0++;
+            } else if(nums[i] == 1){
+                num1++;
+            } else if(nums[i] == 2){
+                num2++;
+            }
+        }
+        
+        // Replace the array with 0, then 1, then 2
+        for(int i = 0; i < num0; i++){
+            nums[i] = 0;
+        }
+        
+        for(int i = 0; i < num1; i++){
+            nums[i + num0] = 1;
+        }
+        
+        for(int i = 0; i < num2; i++){
+            nums[i + num0 + num1] = 2;
+        }
+    }
+}
